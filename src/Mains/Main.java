d08b5f80790bc98cfe1a60d0df298093e4300b45
adrien <adrien.chevrier.pro@gmail.com>
2017-02-20 10:19:41 +0100

    program finishes

--- src/Mains/Main.java
+++ src/Mains/Main.java
@@ -26,6 +26,7 @@ public class Main {
             thread2.join();
             thread3.join();
             thread4.join();
+            System.out.println("Program finishes");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
