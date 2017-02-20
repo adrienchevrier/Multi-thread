11777c4e9b0992668f0e12aaa0113d390536565e
adrien <adrien.chevrier.pro@gmail.com>
2017-02-20 10:13:27 +0100

    improvement

---
+++ src/CounterThread/CounterThread.java
@@ -0,0 +1,31 @@
+package CounterThread;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static sun.management.snmp.jvminstr.JvmThreadInstanceEntryImpl.ThreadStateMap.Byte0.runnable;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class CounterThread implements Runnable {
+
+        // we implement Runnable.
+        @Override
+        public void run() {
+            Thread thread = Thread.currentThread();
+            for (int cpt = 1; cpt <1001; cpt++)
+
+                System.out.println("cpt value:" +cpt + "thread ID:" +thread.getId());
+
+
+        
+        // We observe that each thread doesn't run to the end 
+        // like for example some threads stop at 780 value, and another one is scheduled
+        // so we think the program doesn't do what it has to since threads stop eachother before the end of each
+        
+    }
+    
+}
---
+++ src/Mains/Main.java
@@ -0,0 +1,33 @@
+package Mains;
+
+import CounterThread.CounterThread;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CounterThread t1 = new CounterThread();
+        CounterThread t2 = new CounterThread();
+        CounterThread t3 = new CounterThread();
+        CounterThread t4 = new CounterThread();
+
+        Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread (t2);
+        Thread thread3 = new Thread (t3);
+        Thread thread4 = new Thread (t4);
+        // each of the thread calls the run method by doing thread.start()
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    };
+}
